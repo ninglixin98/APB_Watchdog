805c6d8a62592f050cd7768abde704026f068f41
ning lixin <105833426+ninglixin98@users.noreply.github.com>
2022-07-08 16:36:36 +0800

    Add files via upload

---
+++ v2x_addon_v0.2/cmsdk_apb_watchdog/uvm/sim/csrc/uvm_dpi.d
@@ -0,0 +1,15 @@
+uvm_dpi.o: \
+ /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/etc/uvm-1.2/dpi/uvm_dpi.cc \
+ /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/etc/uvm-1.2/dpi/uvm_dpi.h \
+ /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/include/sv_vpi_user.h \
+ /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/include/vpi_user.h \
+ /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/include/veriuser.h \
+ /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/include/svdpi.h \
+ /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/etc/uvm-1.2/dpi/uvm_common.c \
+ /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/etc/uvm-1.2/dpi/uvm_regex.cc \
+ /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/etc/uvm-1.2/dpi/uvm_hdl.c \
+ /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/etc/uvm-1.2/dpi/uvm_hdl_vcs.c \
+ /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/include/vcsuser.h \
+ /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/include/mhpi_user.h \
+ /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/include/vhpi_user.h \
+ /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/etc/uvm-1.2/dpi/uvm_svcmd_dpi.c
