6d04a038028b87d5dd5e911204712fd146565607
ning lixin <105833426+ninglixin98@users.noreply.github.com>
2022-07-08 16:59:49 +0800

    Add files via upload

---
+++ v2x_addon_v0.3/cmsdk_apb_watchdog/uvm/sim/csrc/uvm_verdi_dpi.d
@@ -0,0 +1,8 @@
+uvm_verdi_dpi.o: \
+ /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/etc/uvm-1.2/verdi/dpi/uvm_verdi_dpi.cpp \
+ /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/include/vpi_user.h \
+ /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/include/veriuser.h \
+ /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/include/sv_vpi_user.h \
+ /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/include/vpi_user.h \
+ /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/include/vcs_vpi_user.h \
+ /opt/synopsys/vcs/vcs-mx/O-2018.09-SP2/include/sv_vpi_user.h
